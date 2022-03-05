99dea4d031db64cf11e43871ae937451dc439e98
JoseMalabuyoc <jvmalabuyoc@hotmail.co.uk>
2022-03-05 20:49:59 +0000

    first commit

--- csvparser.py
+++ csvparser.py
@@ -1,11 +1,15 @@
 def read_row(file):
     next_line = file.readline()
-
     if len(next_line) > 0:
         return parse_row(next_line)
 
     return None
+    print(f'{row[0]} - {row[1]} - {row[2]}')
 
 def parse_row(next_line):
-    return ''
+    newLine = next_line.replace(',', ' - ').replace('\n','')
+    print(newLine)
+    return newLine
+
+
 
