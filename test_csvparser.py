826a94ce54424286941c7a080dc79a093e4781f4
JoseMalabuyoc <95856768+JoseMalabuyoc@users.noreply.github.com>
2022-03-07 13:38:31 +0000

    Update test_csvparser.py

--- test_csvparser.py
+++ test_csvparser.py
@@ -5,10 +5,13 @@ import csvparser
 class TestParser(unittest.TestCase):
     def test_read_row(self):
         file = Mock()
+        # Setup/Configuration
         file.configure_mock(**{'readline.return_value':'\'Vehicle1\',\'Vehicle2\''})
+        # Expected output
         expected = ("'Vehicle1' - 'Vehicle2'")
         result = csvparser.read_row(file)
         print(result)
+        # Assertion
         assert result == expected
     
 if __name__ == "__main__":
